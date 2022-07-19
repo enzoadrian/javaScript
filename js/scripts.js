f2e875b49034634a12a1fa0cd44c1a80caf3c711
enzoag99 <enzo.gasparini@hotmail.com>
2022-07-18 22:29:02 -0300

    agregandoArrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+const carrito = []
+console.log(carrito)
 function agregarAlCarrito(producto, stock){
     const tenemosStock = hayStock(stock);
 
     if(tenemosStock == "tenemos stock"){
-    console.log("agregaste al carrito " + producto)}
+        carrito.push(producto);
+        console.log("agregaste al carrito " + producto)}
 
     else
     {console.log("Lo siento , no nos quedo " + producto)}
@@ -16,16 +19,23 @@ function hayStock(stock){
         return "no nos queda stock"
     }
 }
+
 let stockZapatillas = prompt("cuantas zapatillas hay en stock?")
 let stockGorras = prompt("cuantos gorras hay en stock")
 let stockCamperas = prompt("cuantas camperas hay en  stock ")
-const productos = ["zapatillas air force","gorra NB","campera norton"]
 
-for(let i =0;i<3;i++){console.log(productos[i])
-    
+// Agregando arrays y metodos
+const ropa = ["zapatillas air force","gorra NB","campera norton"]
+const accesorios = ["anillo gold","pulsera island"]
+const productos = ropa.concat(accesorios)
+ropa.push("buzo tyson")
+console.log(ropa.length)
+alert(" estos son nuestro productos a la venta " + productos)
+ 
+for(let i = 0 ; i < ropa.length; i++ ){
+    console.log(ropa[i]) 
 }
-
-    
+   
 agregarAlCarrito("zapatillas air force",stockZapatillas);
 agregarAlCarrito("gorra NB", stockGorras);
 agregarAlCarrito("campera norton", stockCamperas)
