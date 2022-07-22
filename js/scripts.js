dad9462639b438e84506f5dda77e5e0578136f3c
enzoag99 <enzo.gasparini@hotmail.com>
2022-07-21 21:49:17 -0300

    primeraEntegaProyectoJs

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 const carrito = []
 console.log(carrito)
-function agregarAlCarrito(producto, stock){
+function agregarAlCarrito(producto, stock,){
     const tenemosStock = hayStock(stock);
 
     if(tenemosStock == "tenemos stock"){
         carrito.push(producto);
-        console.log("agregaste al carrito " + producto)}
+        console.log("agregaste al carrito " + producto.nombre)}
 
     else
-    {console.log("Lo siento , no nos quedo " + producto)}
+    {console.log("Lo siento , no nos quedo " + producto.nombre)}
 }
+
+
+
+let stockZapatillas = prompt("cuantas zapatillas hay en stock?")
+let stockGorras = prompt("cuantos gorras hay en stock")
+let stockCamperas = prompt("cuantas camperas hay en  stock ")
 
 function hayStock(stock){
     if(stock > 0 ){
@@ -20,22 +26,37 @@ function hayStock(stock){
     }
 }
 
-let stockZapatillas = prompt("cuantas zapatillas hay en stock?")
-let stockGorras = prompt("cuantos gorras hay en stock")
-let stockCamperas = prompt("cuantas camperas hay en  stock ")
+const ropa = [
+    {nombre:"zapatillas air force", precio: 23000 },
+    {nombre:"gorra NB", precio: 2500 },
+    {nombre:"campera norton", precio: 15000 }
+]
+const accesorios = [
+    {nombre:"anillo gold", precio: 1200 },
+    {nombre:"pulsera island",precio: 900 }
+]
 
-// Agregando arrays y metodos
-const ropa = ["zapatillas air force","gorra NB","campera norton"]
-const accesorios = ["anillo gold","pulsera island"]
+ropa.push({nombre:"buzo tyson", precio: 9500 })
+
 const productos = ropa.concat(accesorios)
-ropa.push("buzo tyson")
+
 console.log(ropa.length)
-alert(" estos son nuestro productos a la venta " + productos)
+
+
+productos.forEach( (el) => {
+    console.log(el.nombre)
+} )
  
-for(let i = 0 ; i < ropa.length; i++ ){
-    console.log(ropa[i]) 
-}
-   
-agregarAlCarrito("zapatillas air force",stockZapatillas);
-agregarAlCarrito("gorra NB", stockGorras);
-agregarAlCarrito("campera norton", stockCamperas)
+const resultado = ropa.filter((el) => el.precio <10000)
+   console.log(resultado)
+
+
+agregarAlCarrito((productos[0]),stockZapatillas);
+agregarAlCarrito(productos[1], stockGorras);
+agregarAlCarrito(productos[2], stockCamperas);
+
+const total = carrito.reduce((acumulador
+    , el) => acumulador + el.precio , 0 );
+   console.log(total)
+
+
