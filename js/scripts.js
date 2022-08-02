1d6fa24a75479082d6db7304c71eb16a53cb04da
enzoag99 <enzo.gasparini@hotmail.com>
2022-08-02 20:26:54 -0300

    entrega

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,44 @@
 const carrito = []
-console.log(carrito)
-function agregarAlCarrito(producto, stock,){
-    const tenemosStock = hayStock(stock);
-
-    if(tenemosStock == "tenemos stock"){
-        carrito.push(producto);
-        console.log("agregaste al carrito " + producto.nombre)}
-
-    else
-    {console.log("Lo siento , no nos quedo " + producto.nombre)}
-}
-
-
-
-let stockZapatillas = prompt("cuantas zapatillas hay en stock?")
-let stockGorras = prompt("cuantos gorras hay en stock")
-let stockCamperas = prompt("cuantas camperas hay en  stock ")
-
-function hayStock(stock){
-    if(stock > 0 ){
-        return "tenemos stock";
-    }
-    else{
-        return "no nos queda stock"
-    }
-}
 
 const ropa = [
-    {nombre:"zapatillas air force", precio: 23000 },
-    {nombre:"gorra NB", precio: 2500 },
-    {nombre:"campera norton", precio: 15000 }
+    {id:1,nombre:"zapatillas air force", precio: 23000 },
+    {id:2, nombre:"remera toronto", precio: 2500 },
+    {id:3, nombre:"campera norton", precio: 15000 }
 ]
 const accesorios = [
-    {nombre:"anillo gold", precio: 1200 },
-    {nombre:"pulsera island",precio: 900 }
+    {id:4,nombre:" gorra NB", precio: 1600 },
+    {id:5,nombre:"pulsera island",precio: 900 }
 ]
 
-ropa.push({nombre:"buzo tyson", precio: 9500 })
+ropa.push({id:6, nombre:"buzo tyson", precio: 9500 })
 
 const productos = ropa.concat(accesorios)
 
-console.log(ropa.length)
+function agregarAlCarrito(nombreProducto){
+    alert("Agregaste " + nombreProducto)}
 
 
-productos.forEach( (el) => {
-    console.log(el.nombre)
-} )
- 
 const resultado = ropa.filter((el) => el.precio <10000)
    console.log(resultado)
 
-
-agregarAlCarrito((productos[0]),stockZapatillas);
-agregarAlCarrito(productos[1], stockGorras);
-agregarAlCarrito(productos[2], stockCamperas);
 
 const total = carrito.reduce((acumulador
     , el) => acumulador + el.precio , 0 );
    console.log(total)
 
 
+productos.forEach((producto) => {
+    const idBoton = `agregarCarrito ${producto.id}`
+    document.getElementById("cardsProducts").innerHTML +=
+     `<div class='card d-flex  col-4'>
+      <h2>${producto.nombre}</h2>
+      <button id="${idBoton}">Agregar al carrito</button>
+      </div>`
+})
+/* agregando evento  */
+productos.forEach((producto) =>{
+    const idBoton = `agregarCarrito ${producto.id}`
+    document.getElementById(idBoton).addEventListener  ('click',() => {
+        alert(`agregaste ${producto.nombre}`)}
+        )
+        })
