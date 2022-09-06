30d3b7d235655538e5ae5be0416ce9bd643a1b0f
enzo <enzo.gasparini@hotmail.com>
2022-09-05 21:50:00 -0300

    generando cards con fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,120 +1,89 @@
 
 const carrito= JSON.parse(localStorage.getItem("carrito")) ?? []; 
-const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
-localStorage.setItem("carrito", JSON.stringify(carrito));
-document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;
- 
- 
-const ropa = [
-    {id:1,nombre:"Zapatillas air force",img:"https://essential.vtexassets.com/arquivos/ids/571599-500-auto?v=637909172164170000&width=500&height=auto&aspect=true", precio: 23000,categoria:"zapatillas" },
-    {id:2, nombre:"Remeras toronto",img:"https://http2.mlstatic.com/D_NQ_NP_675234-MLA45098508489_032021-O.webp", precio: 2500,categoria:"ropa"  },
-    {id:3, nombre:"Camperas norton",img:"https://lamoto.com.ar/wp-content/uploads/2019/08/CITY_NEGRA_FRENTE.jpg", precio: 15000,categoria:"ropa" }
-]
-const accesorios = [
-    {id:4,nombre:"Gorra NB",img:"https://www.newbalance.com.ar/media/catalog/product/cache/4/thumbnail/9df78eab33525d08d6e5fb8d27136e95/m/h/mh734103bk_2.jpg", precio: 1600,categoria:"accesorios"  },
-    {id:5,nombre:"Pulsera island",img:"https://http2.mlstatic.com/D_NQ_NP_750431-MLA48925303204_012022-O.webp",precio: 900,categoria:"accesorios"  }
-]
+datosCarrito()
 
-ropa.push({id:6, nombre:"Buzo tyson",img:"https://http2.mlstatic.com/D_NQ_NP_621591-MLA45417531268_042021-O.webp", precio: 9500,categoria:"ropa"  })
+const productos = []
 
-const productos = ropa.concat(accesorios)
-//usando fetch para mostrar los productos en "determinados lugares"(en este caso puntual en la consola)
-const mostrarProductos=()=>{
+const mostrarProductos= ()=>{
   fetch('productos.json')
   .then (response => response.json())
-    .then (info => {console.log(info)
-})}
-mostrarProductos()
+    .then (info => {
+      crearCards(info)
+      agregarAlCarrito(info)
+      productos.push(...info)
+    })}
 
-productos.forEach((producto) => {
+    mostrarProductos()
+
+function crearCards(array){
+array.forEach((producto) => {
     const idBoton = `agregarCarrito ${producto.id}`
     document.getElementById("cardsProducts").innerHTML +=
      `<div class="cards">
-      <img src = ${producto.img} style= "height:200px";"width:150px">
+      <img src = ${producto.img} id="imgCards">
       <h2 class="tituloCard">${producto.nombre}</h2>
       <p>$${producto.precio}</p>
       <button class="botonAgregar"id="${idBoton}">Agregar al carrito</button>
-      </div>`
-})
+      </div>`})}
 
-productos.forEach((producto) =>{
+function agregarAlCarrito(array){
+array.forEach((producto) =>{
     const idBoton = `agregarCarrito ${producto.id}`
     document.getElementById(idBoton).addEventListener  ('click',() => {
         carrito.push(producto);
-        const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;
+        datosCarrito();
         Toastify({
           text: `Agregaste ${producto.nombre} al carrito`,
-          duration: 2000,
+          duration: 1500,
           position:"center",
           }).showToast();
       } )
-        })
+        })}
+      
 
-
-//funcion de filtrado por categoria
 function filtrarPorCategoria(categoria){
       document.getElementById("cardsProducts").innerHTML = ""
-      const productosFiltrados = productos.filter((producto) => producto.categoria === categoria)   
-      productosFiltrados.forEach((producto) => {
-        const idBoton = `agregarCarrito ${producto.id}`
-        document.getElementById("cardsProducts").innerHTML +=
-         `<div class='card d-flex  col-4'>
-          <h2>${producto.nombre}</h2>
-          <img src = ${producto.img} style="width:200px">
-          <p>$${producto.precio}</p>
-          <button id="${idBoton}">Agregar al carrito</button>
-          </div>`
-    })}
+      const productosFiltrados =  productos.filter((producto) => producto.categoria === categoria)   
+       crearCards(productosFiltrados)
+       agregarAlCarrito(productosFiltrados)
+       }
     
-// funcion para mostrar los elementos del carrito
   function mostrarCarrito(){
     document.getElementById("cardsModal").innerHTML = "" 
     carrito.forEach((producto) =>
     {document.getElementById("cardsModal").innerHTML +=
     `<div>
    <p> ${producto.nombre}</p>
-   <img src="${producto.img}" style="width:80px">
+   <img src="${producto.img}" id="imgCarrito">
    <p>$${producto.precio}</p>
    <button id="botonEliminar" type="button" onclick="borrarDelCarrito(${producto.id})">borrar producto </button>
-   </div>`
-  })}
+   </div>`})}
 
+function datosCarrito(){
+  const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;}
 
-   //funcion para borrar elementos del carrito
-   function borrarDelCarrito(idProducto) {
+function borrarDelCarrito(idProducto) {
        const index = carrito.findIndex((producto) => producto.id === idProducto)
        if (index != -1){
        carrito.splice(index, 1)}
        mostrarCarrito()
-       const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
-       localStorage.setItem("carrito", JSON.stringify(carrito));
-       document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;
-      }
+       datosCarrito();}
 
-    
  function comprar(){
       {Swal.fire({
         icon: 'success',
-        title: 'Gracias por tu compra !',
-    })}
+        title: 'Gracias por tu compra !',})}
   carrito.splice(0,carrito.length)
   mostrarCarrito()
-  const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-  document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;
-  }
+  datosCarrito();}
 
   function vaciarCarrito(){
     {Swal.fire({
       icon: 'warning',
       title: 'Tu carrito está vacio',
-      text:'Puedes volver a agregar tus productos cuando desees!'
-  })}
+      text:'Puedes volver a agregar tus productos cuando desees!'})}
     carrito.splice(0,carrito.length)
-  mostrarCarrito()
-  const total= carrito.reduce((acc,producto) =>acc + producto.precio ,0);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-  document.getElementById("cart-number").innerHTML = carrito.length + " - $"+total;
-  }
+  mostrarCarrito();
+  datosCarrito();}
